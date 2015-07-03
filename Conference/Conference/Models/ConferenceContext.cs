8ee9f6f084cdfa7411be70bb11b5c1c144aa16df
Andrew McGregor <burnsmcgregor@outlook.com>
2015-07-03 19:30:26 +0100

    Added Session and Speaker Classes

---
+++ Conference/Conference/Models/ConferenceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conference.Models
+{
+
+    public class ConferenceContext : DbContext
+    {
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+
+        
+    }
+
+}
