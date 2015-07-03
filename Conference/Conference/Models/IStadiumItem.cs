bec2a5f8f6483faf1b6a4feaa91d976f2f1bcbbe
Andrew McGregor <burnsmcgregor@outlook.com>
2015-07-03 19:14:42 +0100

    Added Stadium Models

---
+++ Conference/Conference/Models/IStadiumItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conference.Models
+{
+
+    interface IStadiumItem
+    {
+
+        string Id { get; set; }
+        string TeamName { get; set; }
+        string Formed { get; set; }
+        string StadiumName { get; set; }
+        string Address { get; set; }
+        string Postcode { get; set; }
+        string City { get; set; }
+        string SmallLogo { get; set; }
+        string LargeLogo { get; set; }
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+        string Website { get; set; }
+
+    }
+
+}
