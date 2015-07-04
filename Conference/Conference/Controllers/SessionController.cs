b4fa856d03b5e0da14c5c69d4607c5016e1f8fec
Andrew McGregor <burnsmcgregor@outlook.com>
2015-07-04 22:30:25 +0100

    Controllers

---
+++ Conference/Conference/Controllers/SessionController.cs
@@ -0,0 +1,53 @@
+﻿using Conference.Models;
+using Conference.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Conference.Controllers
+{
+
+    public class SessionController : Controller
+    {
+        // GET: Session
+        public ActionResult Index()
+        {
+            List<Session> sessions = SessionService.GetSessionList();
+            return View(sessions);
+        }
+
+        //  Get: Session/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //  Posr: Session/Create
+        [HttpPost]
+        public ActionResult Create(Session session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(session);
+            }
+            using (var ctx = new ConferenceContext())
+            {
+                try
+                {
+                    SessionService.AddSession(session);
+                    TempData["Message"] = "Created" + session.Title;
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("Error", ex.Message);
+                    return View(session);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+
+}
