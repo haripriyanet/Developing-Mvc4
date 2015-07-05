f99747f29c85af9c215e317bfd147caa42eac976
Andrew McGregor <burnsmcgregor@outlook.com>
2015-07-05 13:47:02 +0100

    Added Session & Speaker Models

---
+++ Conference/Conference/Models/Session.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conference.Models
+{
+
+    public class Session
+    {
+        [Key]
+        public int SessionId { get; set; }
+        [Required()]
+        public string Title { get; set; }
+        [Required(),
+        DataType(DataType.MultilineText)]
+        public string Abstract { get; set; }
+        public int SpeakerId { get; set; }
+        public virtual Speaker Speaker { get; set; }
+    }
+
+}
---
+++ Conference/Conference/Models/Speaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conference.Models
+{
+
+    public class Speaker
+    {
+        [Key]
+        public int SpeakerId { get; set; }
+        [Display(Name = "Speaker")]
+        public string Name { get; set; }
+        [DataType(DataType.EmailAddress),
+        Display(Name = "Email")]
+        public string EmailAddress { get; set; }
+        public virtual List<Session> Sessions { get; set; }
+    }
+
+}
