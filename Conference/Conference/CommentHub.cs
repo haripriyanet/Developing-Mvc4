26915fdbe40f58ea315586752047c8d5320f8879
Andrew McGregor <burnsmcgregor@outlook.com>
2015-07-10 18:13:10 +0100

    Sneek Peek 20:00

---
+++ Conference/Conference/CommentHub.cs
@@ -0,0 +1,28 @@
+﻿using Conference.Models;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conference
+{
+
+    public class CommentHub : Hub
+    {
+
+        public void AddComment(int sessionid, string content)
+        {
+            using (var ctx = new ConferenceContext())
+            {
+                Comment comment = new Comment { SessionId = sessionid, Content = content };
+                ctx.Comments.Add(comment);
+
+                Clients.Group(sessionid.ToString());
+            }
+        }
+
+    }
+
+}
